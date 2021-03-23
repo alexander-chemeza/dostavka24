9049c41ae2a77f271b0e0689eeb783e8a653902f
alexander-chemeza <alexander.chemeza@gmail.com>
2021-03-23 23:27:29 +0300

    routing for reg form and sidebar beta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./pages/login/login.component";
+import {RecoverComponent} from "./pages/recover/recover.component";
+import {RegistrationComponent} from "./pages/registration/registration.component";
+import {CalculatorComponent} from "./pages/calculator/calculator.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'recover', component: RecoverComponent},
+  {path: 'registration', component: RegistrationComponent},
+  {path: 'calculator', component: CalculatorComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {relativeLinkResolution: 'legacy'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import {FormsModule} from "@angular/forms";
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RecoverComponent } from './pages/recover/recover.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { CalculatorComponent } from './pages/calculator/calculator.component';
+
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    LoginComponent,
+    RecoverComponent,
+    RegistrationComponent,
+    CalculatorComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
