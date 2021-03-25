ebbe13a66f09b46bcb9fc28775b98f35ad9b37f0
alexander-chemeza <alexander.chemeza@gmail.com>
2021-03-26 00:02:24 +0300

    registration form events and styles

--- src/app/pages/registration/registration.component.ts
+++ src/app/pages/registration/registration.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
+  agreement = false
+  personNoAgreement = false
+  personWithAgreement = false
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleNoPerson(event) {
+    const controls = document.getElementsByClassName('no-agreement-control') as HTMLCollectionOf<HTMLLinkElement>
+    for(let c of Array.from(controls)) {
+      c.disabled = false
+    }
+    event.target.disabled = true
+    this.personNoAgreement = !this.personNoAgreement
+  }
+
+  toggleWithPerson(event) {
+    const controls = document.getElementsByClassName('with-agreement-control') as HTMLCollectionOf<HTMLLinkElement>
+    for(let c of Array.from(controls)) {
+      c.disabled = false
+    }
+    event.target.disabled = true
+    this.personWithAgreement = !this.personWithAgreement
   }
 
 }
