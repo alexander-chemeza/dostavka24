9080ed8c9ca9624ec4947e74a53ce66d3a372a32
alexander-chemeza <alexander.chemeza@gmail.com>
2021-04-04 19:37:53 +0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,19 +16,20 @@ import {UsersComponent} from './pages/users/users.component';
 import {AuthGuard} from '../auth.guard';
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent},
+  {path: 'login', component: LoginComponent, children: [
+      {path: 'calculator', component: CalculatorComponent},
+      {path: 'book', component: BookComponent, canActivate: [AuthGuard]},
+      {path: 'docs', component: DocsComponent, canActivate: [AuthGuard]},
+      {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
+      {path: 'instructions', component: InstructionsComponent, canActivate: [AuthGuard]},
+      {path: 'journal', component: JournalComponent, canActivate: [AuthGuard]},
+      {path: 'order', component: OrderComponent, canActivate: [AuthGuard]},
+      {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]},
+      {path: 'template', component: TemplateComponent, canActivate: [AuthGuard]},
+      {path: 'users', component: UsersComponent, canActivate: [AuthGuard]}
+    ]},
   {path: 'recover', component: RecoverComponent},
   {path: 'registration', component: RegistrationComponent},
-  {path: 'calculator', component: CalculatorComponent},
-  {path: 'book', component: BookComponent, canActivate: [AuthGuard]},
-  {path: 'docs', component: DocsComponent, canActivate: [AuthGuard]},
-  {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
-  {path: 'instructions', component: InstructionsComponent, canActivate: [AuthGuard]},
-  {path: 'journal', component: JournalComponent, canActivate: [AuthGuard]},
-  {path: 'order', component: OrderComponent, canActivate: [AuthGuard]},
-  {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]},
-  {path: 'template', component: TemplateComponent, canActivate: [AuthGuard]},
-  {path: 'users', component: UsersComponent, canActivate: [AuthGuard]},
   {path: '', redirectTo: 'login', pathMatch: 'full'}
 ];
 
