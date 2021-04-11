f4c6c1635487b62b451ee2cff3cea3af8aa15379
alexander-chemeza <alexander.chemeza@gmail.com>
2021-04-11 15:06:42 +0300

    book pagination

--- src/app/pages/book/book.component.ts
+++ src/app/pages/book/book.component.ts
@@ -6,38 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book.component.scss']
 })
 export class BookComponent implements OnInit {
-  columnDefsContrAgent = [
-    { headerName: 'Наименование', field: 'name', sortable: true, flex: 1}
-  ];
-
-  rowDataContrAgent = [
-    { name: 'Верхнедвинская районная ветеринарная станция' },
-    { name: 'Верхнедвинская районная ветеринарная станция' },
-    { name: 'Верхнедвинская районная ветеринарная станция' },
-    { name: 'Верхнедвинская районная ветеринарная станция' },
-    { name: 'Верхнедвинская районная ветеринарная станция' },
-    { name: 'Верхнедвинская районная ветеринарная станция' },
-    { name: 'Верхнедвинская районная ветеринарная станция' },
-    { name: 'Верхнедвинская районная ветеринарная станция' },
-    { name: 'Верхнедвинская районная ветеринарная станция' },
-    { name: 'Верхнедвинская районная ветеринарная станция' },
-    { name: 'Верхнедвинская районная ветеринарная станция' },
-    { name: 'Верхнедвинская районная ветеринарная станция' },
-    { name: 'Верхнедвинская районная ветеринарная станция' },
-    { name: 'Верхнедвинская районная ветеринарная станция' },
-  ];
-
+  // Grid
   public gridApi;
   public gridColumnApi;
-
-  public columnDefsAddress;
-  public columnDefsContacts;
+  public autoGroupColumnDef;
   public defaultColDef;
   public rowSelection;
+  public rowGroupPanelShow;
+  public pivotPanelShow;
+  public paginationPageSize;
+  public paginationNumberFormatter;
+  // Columns
+  public columnDefsContrAgent;
+  public columnDefsAddress;
+  public columnDefsContacts;
+  // Rows
+  public rowDataContrAgent;
   public rowDataAddress;
   public rowDataContacts;
 
   constructor() {
+    // Columns
+    this.columnDefsContrAgent = [
+      { headerName: 'Наименование', field: 'name', sortable: true, flex: 1}
+    ];
     this.columnDefsAddress = [
       {headerName: 'Основной',
         field: 'main',
@@ -51,23 +43,6 @@ export class BookComponent implements OnInit {
       },
       {headerName: 'Наименование', field: 'name', sortable: true, filter: true, flex: 3}
     ];
-
-    this.rowDataAddress = [
-      {name: 'г. Брест, УЛ. СОВЕТСКИХ ПОГРАНИЧНИКОВ, д. 1, оф. 1'},
-      {name: 'г. Брест, УЛ. СОВЕТСКИХ ПОГРАНИЧНИКОВ, д. 1, оф. 1'},
-      {name: 'г. Брест, УЛ. СОВЕТСКИХ ПОГРАНИЧНИКОВ, д. 1, оф. 1'},
-      {name: 'г. Брест, УЛ. СОВЕТСКИХ ПОГРАНИЧНИКОВ, д. 1, оф. 1'},
-      {name: 'г. Брест, УЛ. СОВЕТСКИХ ПОГРАНИЧНИКОВ, д. 1, оф. 1'},
-      {name: 'г. Брест, УЛ. СОВЕТСКИХ ПОГРАНИЧНИКОВ, д. 1, оф. 1'},
-      {name: 'г. Брест, УЛ. СОВЕТСКИХ ПОГРАНИЧНИКОВ, д. 1, оф. 1'},
-      {name: 'г. Брест, УЛ. СОВЕТСКИХ ПОГРАНИЧНИКОВ, д. 1, оф. 1'},
-      {name: 'г. Брест, УЛ. СОВЕТСКИХ ПОГРАНИЧНИКОВ, д. 1, оф. 1'},
-      {name: 'г. Брест, УЛ. СОВЕТСКИХ ПОГРАНИЧНИКОВ, д. 1, оф. 1'},
-      {name: 'г. Брест, УЛ. СОВЕТСКИХ ПОГРАНИЧНИКОВ, д. 1, оф. 1'},
-      {name: 'г. Брест, УЛ. СОВЕТСКИХ ПОГРАНИЧНИКОВ, д. 1, оф. 1'},
-      {name: 'г. Брест, УЛ. СОВЕТСКИХ ПОГРАНИЧНИКОВ, д. 1, оф. 1'}
-    ];
-
     this.columnDefsContacts = [
       { headerName: 'Имя контакта',
         field: 'contactNameCheck',
@@ -128,7 +103,38 @@ export class BookComponent implements OnInit {
         maxWidth: 400
       }
     ];
-
+    // Rows
+    this.rowDataContrAgent = [
+      { name: 'Верхнедвинская районная ветеринарная станция' },
+      { name: 'Верхнедвинская районная ветеринарная станция' },
+      { name: 'Верхнедвинская районная ветеринарная станция' },
+      { name: 'Верхнедвинская районная ветеринарная станция' },
+      { name: 'Верхнедвинская районная ветеринарная станция' },
+      { name: 'Верхнедвинская районная ветеринарная станция' },
+      { name: 'Верхнедвинская районная ветеринарная станция' },
+      { name: 'Верхнедвинская районная ветеринарная станция' },
+      { name: 'Верхнедвинская районная ветеринарная станция' },
+      { name: 'Верхнедвинская районная ветеринарная станция' },
+      { name: 'Верхнедвинская районная ветеринарная станция' },
+      { name: 'Верхнедвинская районная ветеринарная станция' },
+      { name: 'Верхнедвинская районная ветеринарная станция' },
+      { name: 'Верхнедвинская районная ветеринарная станция' },
+    ];
+    this.rowDataAddress = [
+      {name: 'г. Брест, УЛ. СОВЕТСКИХ ПОГРАНИЧНИКОВ, д. 1, оф. 1'},
+      {name: 'г. Брест, УЛ. СОВЕТСКИХ ПОГРАНИЧНИКОВ, д. 1, оф. 1'},
+      {name: 'г. Брест, УЛ. СОВЕТСКИХ ПОГРАНИЧНИКОВ, д. 1, оф. 1'},
+      {name: 'г. Брест, УЛ. СОВЕТСКИХ ПОГРАНИЧНИКОВ, д. 1, оф. 1'},
+      {name: 'г. Брест, УЛ. СОВЕТСКИХ ПОГРАНИЧНИКОВ, д. 1, оф. 1'},
+      {name: 'г. Брест, УЛ. СОВЕТСКИХ ПОГРАНИЧНИКОВ, д. 1, оф. 1'},
+      {name: 'г. Брест, УЛ. СОВЕТСКИХ ПОГРАНИЧНИКОВ, д. 1, оф. 1'},
+      {name: 'г. Брест, УЛ. СОВЕТСКИХ ПОГРАНИЧНИКОВ, д. 1, оф. 1'},
+      {name: 'г. Брест, УЛ. СОВЕТСКИХ ПОГРАНИЧНИКОВ, д. 1, оф. 1'},
+      {name: 'г. Брест, УЛ. СОВЕТСКИХ ПОГРАНИЧНИКОВ, д. 1, оф. 1'},
+      {name: 'г. Брест, УЛ. СОВЕТСКИХ ПОГРАНИЧНИКОВ, д. 1, оф. 1'},
+      {name: 'г. Брест, УЛ. СОВЕТСКИХ ПОГРАНИЧНИКОВ, д. 1, оф. 1'},
+      {name: 'г. Брест, УЛ. СОВЕТСКИХ ПОГРАНИЧНИКОВ, д. 1, оф. 1'}
+    ];
     this.rowDataContacts = [
       {contactName: 'Анна', employee: '-', phone: '+375 33 865 55 78', tel1: '+375 33 865 55 78', tel2: '+375 33 865 55 78', email: 'mail@mail.com'},
       {contactName: 'Анна', employee: '-', phone: '+375 33 865 55 78', tel1: '+375 33 865 55 78', tel2: '+375 33 865 55 78', email: 'mail@mail.com'},
@@ -147,6 +153,22 @@ export class BookComponent implements OnInit {
       {contactName: 'Анна', employee: '-', phone: '+375 33 865 55 78', tel1: '+375 33 865 55 78', tel2: '+375 33 865 55 78', email: 'mail@mail.com'},
       {contactName: 'Анна', employee: '-', phone: '+375 33 865 55 78', tel1: '+375 33 865 55 78', tel2: '+375 33 865 55 78', email: 'mail@mail.com'},
     ];
+    // Grid
+    // this.defaultColDef = {
+    //   // editable: true,
+    //   // // enableRowGroup: true,
+    //   // // enablePivot: true,
+    //   // // enableValue: true,
+    //   sortable: true,
+    //   resizable: true,
+    //   filter: true,
+    //   flex: 1,
+    //   minWidth: 100,
+    // };
+    this.rowSelection = 'multiple';
+    this.rowGroupPanelShow = 'always';
+    this.pivotPanelShow = 'always';
+    this.paginationPageSize = 10;
 
     this.defaultColDef = {
       flex: 1,
@@ -154,15 +176,24 @@ export class BookComponent implements OnInit {
       resizable: true,
       icons: {check: '../../../assets/img/common/checked.svg'}
     };
-    this.rowSelection = 'multiple';
+    this.paginationNumberFormatter = function(params) {
+      return '[' + params.value.toLocaleString() + ']';
+    };
   }
 
   onGridReady(params) {
     this.gridApi = params.api;
     this.gridColumnApi = params.columnApi;
+    console.log(this.gridApi);
   }
 
   ngOnInit(): void {
   }
 
+  onPageSizeChanged(event) {
+    const value = event.target.value;
+    console.log(value);
+    this.gridApi.paginationSetPageSize(Number(value));
+    this.gridApi.paginationGetCurrentPage();
+  }
 }
