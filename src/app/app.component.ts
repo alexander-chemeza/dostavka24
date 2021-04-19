79c4f4dafbd213e9b4e5b91eb321713ab67b09e6
alexander-chemeza <alexander.chemeza@gmail.com>
2021-04-19 18:43:20 +0300

    sidebar change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor() {
+export class AppComponent implements OnInit{
+  state: boolean;
+  constructor(private router: Router) {
+    this.state = false;
+  }
+
+  ngOnInit(): void {
+    // On application load, if user was logged in, navigate to home, else - start page (login)
+    if(localStorage.getItem('currentUser')) {
+      this.router.navigate(['home']);
+    } else {
+      this.router.navigate(['login']);
+    }
   }
 }
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-sidebar',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+  state: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    this.state = false;
   }
 
+  ngOnInit(): void {
+    // On hashchange check if user have this item in the localStorage, true - full sidebar, false - basic
+    this.router.events.forEach(event => {
+      if(event instanceof NavigationEnd) {
+        if(localStorage.getItem('currentUser')) {
+          this.state = true;
+        } else {
+          this.state = false;
+        }
+      }
+    });
+  }
 }
